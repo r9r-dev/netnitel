a3be63249424f6a4a70ff17fb13d63208c4b0e6a
r9r-dev <bold.spot3022@lamour.bzh>
2025-03-20 16:48:01 +0100

    Remove deprecated update script
    - Deleted old batch script for Docker image updates
    - Removed manual version input and tagging process
    - Eliminated redundant Docker push commands

--- update.cmd
+++
@@ -1,8 +0,0 @@
-echo off
-set /p "tag=Enter Version: "
-docker build -t netnitel:%tag% .
-docker tag netnitel:%tag% docker.lamour.bzh/netnitel:%tag%
-docker tag netnitel:%tag% docker.lamour.bzh/netnitel:latest
-docker push docker.lamour.bzh/netnitel:%tag%
-docker push docker.lamour.bzh/netnitel:latest
-pause
