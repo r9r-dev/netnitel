175dcb73c0e0efdc6fff9349f4685f7de2aa7238
Ronan <bold.spot3022@lamour.bzh>
2025-03-23 17:07:59 +0100

    ✨ add MiniColor enum
    Introduce MiniColor enum with basic color options.

---
+++ src/Services/Engine/Enums/MiniColor.cs
@@ -0,0 +1,13 @@
+namespace NetNitel.Services.Engine.Enums;
+
+public enum MiniColor
+{
+    Black = 0,
+    Red = 1,
+    Green = 2,
+    Yellow = 3,
+    Blue = 4,
+    Magenta = 5,
+    Cyan = 6,
+    White = 7
+}
